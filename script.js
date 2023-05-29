a220bcb8425fa11ee8f80af52c103557fea9a28c
kauã <kauaxavier032@gmail.com>
2023-05-29 12:43:41 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-let btn = document.getElementById('continue')
-const objects = fetch('./data.json')
+let alldivs = document.getElementById('alldivs')
 
-console.log(objects)
+
+const fetchData = async () => {
+    await fetch("./data.json").then((response) => response.json()).then((data) => {
+       data.map((value) => {
+            alldivs.innerHTML += `
+            <div class="div" style="background-color: ${value.background};">
+                <div class="div1">
+                    <img src="${value.icon}" alt="">
+                    <p style="color: ${value.color};">${value.category}</p>
+                </div>
+                <div class="div2">
+                    <p >${value.score}</p>
+                    <p class="div2P2">/ 100</p>
+                </div>
+            </div>
+            `
+       })
+    });
+}
+
+fetchData();
